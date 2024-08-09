d11ea2f26442b3eac8a6a6718c7f383d9a9d8d73
albertocester96 <albertocester96@gmail.com>
2024-08-09 15:18:13 +0200

    typescript and navigation set up

---
+++ app/navigation.tsx
@@ -0,0 +1,13 @@
+
+const Navigation = () => {
+  return (
+    <div>
+        <p> Logo </p>
+        <h1> Dashboard</h1>
+        <h1> About</h1>
+      
+    </div>
+  )
+}
+
+export default Navigation
