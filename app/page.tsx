df180050807c63f5fd16f0c340f6d4fedf156db9
albertocester96 <albertocester96@gmail.com>
2024-08-09 18:31:12 +0200

    set up prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 
 const Home = () => {
 
-  
+
 
   return (
     <div className="flex w-full justify-center pt-3">
       <label className="input input-bordered flex items-center gap-2"> Attività
-        <input type="text" placeholder="Inserisci una attività" />
+        <input type="text" 
+          placeholder="Inserisci una attività" />
       </label>
     </div>
   )
