438b4af50cbcf6f09092626c7b393633ecfdb6d1
albertocester96 <albertocester96@gmail.com>
2024-08-11 09:14:18 +0200

    Submit form with react-hook-form

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,13 +1,24 @@
+"use client"
+
 import React from 'react'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IssueForm {
+    title: string
+    description: string
+}
 
 const NewIssuePage = () => {
+    const {register, handleSubmit} = useForm<IssueForm>();
+    const onSubmit: SubmitHandler<IssueForm> = (data) => console.log(data)
+
   return (
-    <div className="max-w-xl space-y-5">
-    <TextField.Root placeholder="Issue Title" />
-    <TextArea placeholder="Insert the issue description" />
+    <form className="max-w-xl space-y-5" onSubmit={handleSubmit(onSubmit)}>
+    <TextField.Root placeholder="Issue Title" {...register("title")} />
+    <TextArea placeholder="Insert the issue description" {...register("description")} />
     <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
