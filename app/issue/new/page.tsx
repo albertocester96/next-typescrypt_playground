5a1092ee611f2ba7aedd9d07ec840e9c32828a6c
albertocester96 <albertocester96@gmail.com>
2024-08-11 09:41:46 +0200

    handle form submission

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter } from 'next/navigation';
 
 interface IssueForm {
     title: string
@@ -10,8 +11,21 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+    const router = useRouter()
     const {register, handleSubmit} = useForm<IssueForm>();
-    const onSubmit: SubmitHandler<IssueForm> = (data) => console.log(data)
+
+    const onSubmit: SubmitHandler<IssueForm> = async (data) => {
+      try{
+          fetch("/api/issues/", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(data)
+        });
+        router.push('/issue')
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
   return (
     <form className="max-w-xl space-y-5" onSubmit={handleSubmit(onSubmit)}>
